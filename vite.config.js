4cd643a252e7d20b0e07f98534cfed7c6b55e019
majudhu <majudhu.sup@gmail.com>
2021-09-14 20:57:04 +0500

    add vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  base: process.env.NODE_ENV === "production" ? "/learn-vite-vue3-blog" : "/",
+  build: { outDir: "docs" },
+});
