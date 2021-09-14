c27f1e3b458e2c6dbc2b95d127dbbf1b728ec868
majudhu <majudhu.sup@gmail.com>
2021-09-14 22:12:31 +0500

    remove env detection from base path, no need

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === "production" ? "/learn-vite-vue3-blog" : "/",
+  base: "/learn-vite-vue3-blog/",
   build: { outDir: "docs" },
 });
